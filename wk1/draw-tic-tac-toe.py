95cef560e0f0ac632d3b588292553a85ba069dfc
Matt Jones <gitcode@magisa.org>
2013-11-23 23:49:34 -0900

    Some simple drawing functions for tic-tac-toe using the python Turtle package.

---
+++ wk1/draw-tic-tac-toe.py
@@ -0,0 +1,38 @@
+# Drawing functions for tic-tac-toe
+# Matt Jones 2013
+
+import turtle
+
+def drawboard():
+	t = turtle.Turtle()
+	t.pensize(5)
+	t.color('red')
+	t.clear()
+	t.penup()
+	t.goto(100,300)
+	t.pendown()
+	t.goto(100,-300)
+	t.penup()
+	t.goto(-100,-300)
+	t.pendown()
+	t.goto(-100,300)
+	t.penup()
+	t.goto(-300,100)
+	t.pendown()
+	t.goto(300,100)
+	t.penup()
+	t.goto(300,-100)
+	t.pendown()
+	t.goto(-300,-100)
+
+def gotocell(t, cellx, celly):
+	t.penup()
+	originx = -235
+	originy = 140
+	newx = originx + ((cellx-1)*200)
+	newy = originy - ((celly-1)*200)
+	t.goto(newx,newy)
+	t.pendown()
+
+def marktic(t, letter):
+	o.write(letter, font=('Arial', 96, 'normal'))
