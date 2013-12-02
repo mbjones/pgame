4f566c7ec41cf23015a9e98269c6a6c363a89b95
Matt Jones <gitcode@magisa.org>
2013-12-02 13:50:18 -0900

    Changed formatting to spaces.

--- wk1/draw_grid.py
+++ wk1/draw_grid.py
@@ -4,64 +4,64 @@
 import turtle
 
 def drawgrid():
-	"""Draw a 400x400 grid on the screen, label axes, and return the Turtle used to draw the grid."""
+    """Draw a 400x400 grid on the screen, label axes, and return the Turtle used to draw the grid."""
 
-	turtle.setup(width=900, height=900, startx=0, starty=0)
-	t = turtle.Turtle()
-	s = t.getscreen()
-	#s.screensize(420,420)
-	t.hideturtle()
-	t.clear()
-	delay = s.delay()
-	tracer = s.tracer()
-	s.delay(0)
-	s.tracer(0)
-	t.speed(0)
-	maxc = 400
-	for x in range(-1*maxc,maxc+10,10):
-		t.penup()
-		t.goto(x,maxc)
-		t.pendown()
-		if (x % 100 == 0):
-			t.pensize(3)
-		else:
-			t.pensize(1)
-		if (x == 0):
-			t.pencolor('red')
-		else:
-			t.pencolor('black')
-		t.goto(x,-1*maxc)
-	for y in range(maxc,-1*maxc-10,-10):
-		t.penup()
-		t.goto(-1*maxc,y)
-		t.pendown()
-		if (y % 100 == 0):
-			t.pensize(4)
-		else:
-			t.pensize(1)
-		if (y == 0):
-			t.pencolor('red')
-		else:
-			t.pencolor('black')
-		t.goto(maxc,y)
-	labelaxes(t, maxc)
-	s.tracer(tracer)
-	s.delay(delay)
-	t.speed(3)
-	return(t)
+    turtle.setup(width=900, height=900, startx=0, starty=0)
+    t = turtle.Turtle()
+    s = t.getscreen()
+    #s.screensize(420,420)
+    t.hideturtle()
+    t.clear()
+    delay = s.delay()
+    tracer = s.tracer()
+    s.delay(0)
+    s.tracer(0)
+    t.speed(0)
+    maxc = 400
+    for x in range(-1*maxc,maxc+10,10):
+        t.penup()
+        t.goto(x,maxc)
+        t.pendown()
+        if (x % 100 == 0):
+            t.pensize(3)
+        else:
+            t.pensize(1)
+        if (x == 0):
+            t.pencolor('red')
+        else:
+            t.pencolor('black')
+        t.goto(x,-1*maxc)
+    for y in range(maxc,-1*maxc-10,-10):
+        t.penup()
+        t.goto(-1*maxc,y)
+        t.pendown()
+        if (y % 100 == 0):
+            t.pensize(4)
+        else:
+            t.pensize(1)
+        if (y == 0):
+            t.pencolor('red')
+        else:
+            t.pencolor('black')
+        t.goto(maxc,y)
+    labelaxes(t, maxc)
+    s.tracer(tracer)
+    s.delay(delay)
+    t.speed(3)
+    return(t)
 
 def labelaxes(t, maxc):
-	t.penup()
-	t.goto(0, maxc+10)
-	t.write('0')
-	t.goto(-1*maxc-10, maxc+10)
-	t.write('-400, 400')
-	t.goto(maxc+10, maxc+10)
-	t.write('400, 400')
-	t.goto(maxc+10, -1*maxc-10)
-	t.write('400, -400')
-	t.goto(-1*maxc-10, -1*maxc-10)
-	t.write('-400, -400')
-	t.goto(-1*maxc-10, 0)
-	t.write('0')
+    t.penup()
+    t.goto(0, maxc+10)
+    t.write('0')
+    t.goto(-1*maxc-10, maxc+10)
+    t.write('-400, 400')
+    t.goto(maxc+10, maxc+10)
+    t.write('400, 400')
+    t.goto(maxc+10, -1*maxc-10)
+    t.write('400, -400')
+    t.goto(-1*maxc-10, -1*maxc-10)
+    t.write('-400, -400')
+    t.goto(-1*maxc-10, 0)
+    t.write('0')
 
--- wk1/draw_tic_tac_toe.py
+++ wk1/draw_tic_tac_toe.py
@@ -4,36 +4,52 @@
 import turtle
 
 def drawboard():
-	t = turtle.Turtle()
-	t.pensize(5)
-	t.color('red')
-	t.clear()
-	t.penup()
-	t.goto(100,300)
-	t.pendown()
-	t.goto(100,-300)
-	t.penup()
-	t.goto(-100,-300)
-	t.pendown()
-	t.goto(-100,300)
-	t.penup()
-	t.goto(-300,100)
-	t.pendown()
-	t.goto(300,100)
-	t.penup()
-	t.goto(300,-100)
-	t.pendown()
-	t.goto(-300,-100)
+    t = turtle.Turtle()
+    t.pensize(5)
+    t.color('red')
+    t.clear()
+    t.penup()
+    t.goto(100,300)
+    t.pendown()
+    t.goto(100,-300)
+    t.penup()
+    t.goto(-100,-300)
+    t.pendown()
+    t.goto(-100,300)
+    t.penup()
+    t.goto(-300,100)
+    t.pendown()
+    t.goto(300,100)
+    t.penup()
+    t.goto(300,-100)
+    t.pendown()
+    t.goto(-300,-100)
+    return(t);
 
 def gotocell(t, cellx, celly):
-	t.penup()
-	originx = -235
-	originy = 140
-	newx = originx + ((cellx-1)*200)
-	newy = originy - ((celly-1)*200)
-	t.goto(newx,newy)
-	t.pendown()
+    t.penup()
+    originx = -235
+    originy = 140
+    newx = originx + ((cellx-1)*200)
+    newy = originy - ((celly-1)*200)
+    t.goto(newx,newy)
+    t.pendown()
 
 def marktic(t, letter):
-	t.write(letter, font=('Arial', 96, 'normal'))
+    t.write(letter, font=('Arial', 96, 'normal'))
 
+def main():
+    t = drawboard()
+    gotocell(t,1,1)
+    marktic(t, 'X')
+    gotocell(t,2,2)
+    marktic(t, 'O')
+    gotocell(t,3,3)
+    marktic(t, 'X')
+
+    moves = [[0]*3 for x in xrange(3)]
+    turtle.mainloop()
+    
+if __name__ == '__main__':
+    msg = main()
+    
